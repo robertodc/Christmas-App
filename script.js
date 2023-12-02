66cfe0d2a85b86b741d73b13b46498e2db1cd1e0
robertodc <48517474+robertodc@users.noreply.github.com>
2023-12-02 19:37:58 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,57 +1,52 @@
-let currentUser;
-const users = [
-  { username: 'Chiara', gifts: [] },
-  { username: 'Tony', gifts: [] },
-  { username: 'Gelsomina', gifts: [] },
-  { username: 'Sofia', gifts: [] },
-  { username: 'Alice', gifts: [] },
-  { username: 'Lidia', gifts: [] },
-  { username: 'Roberto', gifts: [] }
-];
+// ...
 
-function login() {
+async function login() {
   const usernameSelect = document.getElementById('username');
   const selectedUsername = usernameSelect.value;
-  
+
   if (selectedUsername.trim() !== '') {
-    currentUser = users.find(user => user.username === selectedUsername);
+    const userId = await addUser(selectedUsername);
+    currentUser = { _id: userId, username: selectedUsername, gifts: [] };
     renderWishlist();
   }
 }
 
-function addGift() {
+async function addUser(username) {
+  const response = await fetch('/api/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username }),
+  });
+
+  const data = await response.json();
+  return data.userId;
+}
+
+async function addGift() {
   const giftInput = document.getElementById('add-gift-input');
   const gift = giftInput.value;
-  
+
   if (gift.trim() !== '') {
+    await addGiftToUser(currentUser._id, gift);
     currentUser.gifts.push(gift);
     renderWishlist();
     giftInput.value = '';
   }
 }
 
-function renderWishlist() {
-  const userListContainer = document.getElementById('user-list');
-  userListContainer.innerHTML = '<h3>Wishlist</h3>';
-  
-  users.forEach(user => {
-    if (user !== currentUser) {
-      const userDiv = document.createElement('div');
-      userDiv.textContent = user.username + ': ' + user.gifts.join(', ');
-      userListContainer.appendChild(userDiv);
-    }
+async function addGiftToUser(userId, gift) {
+  await fetch('/api/gifts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userId, gift }),
   });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const usernameSelect = document.getElementById('username');
-  users.forEach(user => {
-    const option = document.createElement('option');
-    option.value = user.username;
-    option.text = user.username;
-    usernameSelect.appendChild(option);
-  });
-
-  const addButton = document.getElementById('add-gift-button');
-  addButton.addEventListener('click', addGift);
-});
+async function loadGiftsFromServer() {
+  const response = await fetch('/api/gifts');
+  const data = await response.json();
+ 
